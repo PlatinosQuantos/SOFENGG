93e97a70112391d3f1e730a4cfe98a162fa5a5cf
Patrick Sy <patrick.alq.sy@gmail.com>
2017-12-04 23:25:53 +0800

    Populate Main Menu combo box with Users

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -1,9 +1,19 @@
 package controller;
 
 import controller.ViewManager.ViewManagerException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.DatabaseModel;
+import model.User;
+import view.dialog.DialogFactory;
 
 import java.io.IOException;
 
@@ -12,9 +22,16 @@ public class MainMenuController extends Controller
     @FXML
     private Button buttonNewOrder, buttonInventory, buttonSettings, buttonFiles, buttonAnalytics;
 
+    @FXML
+    private ComboBox comboName;
+
+    private DatabaseModel dbm;
+
     public MainMenuController() throws IOException
     {
         initialize(this, "/view/main-menu", "/view/main-menu");
+
+        dbm = new DatabaseModel();
     }
 
     @Override
@@ -22,6 +39,8 @@ public class MainMenuController extends Controller
     {
         if(checkInitialLoad(getClass().getSimpleName()))
         {
+            setupComboName();
+
             buttonNewOrder.addEventHandler(ActionEvent.ACTION, e ->
                     viewManager.switchViews("NewOrderController"));
 
@@ -37,11 +56,50 @@ public class MainMenuController extends Controller
             buttonAnalytics.addEventHandler(ActionEvent.ACTION, e ->
                     viewManager.switchViews("AnalyticsController"));
         }
+
+        loadUsers();
     }
 
     @Override
     public void clear()
     {
 
+    }
+
+    private void setupComboName()
+    {
+        Callback<ListView<User>, ListCell<User>> factory = new Callback<ListView<User>, ListCell<User>>()
+        {
+            @Override
+            public ListCell<User> call(ListView<User> param)
+            {
+                return new ListCell<User>()
+                {
+                    @Override
+                    protected void updateItem(User user, boolean empty)
+                    {
+                        super.updateItem(user, empty);
+
+                        if(user != null)
+                            setText(user.getUsername());
+                        else
+                            setText(null);
+                    }
+                };
+            }
+        };
+
+        comboName.setCellFactory(factory);
+        comboName.setButtonCell(factory.call(null));
+        comboName.valueProperty().addListener((ChangeListener<User>) (ov, oldValue, newValue) ->
+        {
+            if(oldValue != null)
+                DialogFactory
+        });
+    }
+
+    private void loadUsers()
+    {
+        comboName.setItems(FXCollections.observableArrayList(dbm.getUsers()));
     }
 }
